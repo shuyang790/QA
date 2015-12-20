e03199497ae272fd393fa09a5f26b3e683d398af
Shuyang Shi <shuyang790@gmail.com>
2015-12-21 00:30:05 +0800

    clean

--- submit/1300012959_1.0/source/answer_retrival/multiprocess/run.py
+++
@@ -1,3 +0,0 @@
-import os
-for i in range(4):
-    os.system("./python extract.py %d %d &" % (i * 10 + 1, i * 10 + 10))
--- submit/1300012959_1.0/source/preprocess/zht2zhs.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python 
-
-import os
-
-for i in range(10):
-    os.system("opencc -i wiki_0" + str(i) + " -o wiki_chs_0" + str(i) + " -c /usr/local/share/opencc/tw2s.json")
-    
